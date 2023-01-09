da726d2f2516e27b7263cdd660ebac78862db795
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-09 17:06:43 +0100

    Initial styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import './normal.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <aside className="sidemenu">
+      <div className="side-menu-button">
+        New chat
+      </div>
+    </aside>
+    <section className="chatbox">
+
+    </section>
+      
     </div>
   );
 }
