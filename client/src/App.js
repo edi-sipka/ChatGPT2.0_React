9520c1a3573c910cb86c4f4300c12d77b7f22e47
edi-sipka@gmail.com <edisipka@gmail.com>
2023-01-12 12:34:26 +0100

    Update styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,26 @@ function App() {
     <div className="App">
     <aside className="sidemenu">
       <div className="side-menu-button">
+      <span> + </span>
         New chat
       </div>
     </aside>
     <section className="chatbox">
+      <div className="chat-log">
+        <div className="chat-message">
+          <div className="avatar">
 
+          </div>
+          <div className="message">
+            
+          </div>
+        </div>
+      </div>
+      <div className="chat-input-holder">
+        <textarea className="chat-input-textarea" 
+        rows="1"
+        ></textarea>
+      </div>
     </section>
       
     </div>
